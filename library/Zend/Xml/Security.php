4641564fc908f6ea3521ce1456769bf7d5486dfb
Dmitry Coolgun <dmitry.coolgun@innowise-group.com>
2022-03-04 12:49:22 +0300

    rewrite php version in security

--- library/Zend/Xml/Security.php
+++ library/Zend/Xml/Security.php
@@ -170,7 +170,7 @@ class Zend_Xml_Security
             version_compare(PHP_VERSION, '5.5.22', 'lt')
             || (
                 version_compare(PHP_VERSION, '5.6', 'gte')
-                && version_compare(PHP_VERSION, '5.6.6', 'lt')
+                && version_compare(PHP_VERSION, '8.0', 'lt')
             )
         );
 
